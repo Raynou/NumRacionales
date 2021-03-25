9bd7b81ac7f7da3df40923186c8c83ca30eca04e
Ray <rafaelgame2012@gmail.com>
2021-03-25 15:37:45 -0600

    second master commit, some fixes

--- src/numracionalesmain/NumRacional.java
+++ src/numracionalesmain/NumRacional.java
@@ -14,10 +14,7 @@ public class NumRacional {
     
     public NumRacional(int numerador, int denominador) {
         this.numerador = numerador;
-        
         this.denominador = denominador;
-       
-        
     }
 
     public int getNumerador() {
@@ -36,49 +33,7 @@ public class NumRacional {
         this.denominador = denominador;
     }
     
-    /*public int [] Sumar(int primNum, int primDen, int segNum, int segDen){
-        int numRacional [] = new int [2];
-                
-        int numTotal = (primNum*segDen)+(primDen*segNum);
-        int denTotal =  primDen * segDen;
-        
-        numRacional[0]=numTotal;
-        numRacional[1]=denTotal;
-        
-        return numRacional;
-    }
-    public int [] Restar(int primNum, int primDen, int segNum, int segDen){
-         int numRacional [] = new int [2];
-        
-        int denTotal =  primDen * segDen;
-        int numTotal = (primNum*segDen)-(primDen*segNum);
-        
-        numTotal = numRacional[0];
-        denTotal = numRacional[1];
-        
-        return numRacional;
-        
-    }*/
-    
-    
-    
-    
     public NumRacional Sumar(NumRacional num, NumRacional num2){
-        /*int numRacional [] = new int [2];
-        int numRacional2 [] = new int [2];
-        int resultado [] = new int [2];
-        
-        numRacional[0] = num.getNumerador();
-        numRacional[1] = num.getDenominador();
-        
-        numRacional2[0] = num2.getNumerador();
-        numRacional2[1] = num2.getDenominador();
-        
-        resultado[0] = (numRacional[0]*numRacional2[1])+(numRacional[1]*numRacional2[0]);
-        resultado[1] =  numRacional[1] * numRacional2[1];
-             
-        return resultado;*/
-
         int a = num.getNumerador();
         int b = num.getDenominador();
         int c = num2.getNumerador();
@@ -88,21 +43,15 @@ public class NumRacional {
         this.setDenominador(b*d);
         return this;
     }
-    public int [] Restar(NumRacional num, NumRacional num2){
-        int numRacional [] = new int [2];
-        int numRacional2 [] = new int [2];
-        int resultado [] = new int [2];
-        
-        numRacional[0] = num.getNumerador();
-        numRacional[1] = num.getDenominador();
-        
-        numRacional2[0] = num2.getNumerador();
-        numRacional2[1] = num2.getDenominador();
-        
-        resultado[0] = (numRacional[0]*numRacional2[1])-(numRacional[1]*numRacional2[0]);
-        resultado[1] =  numRacional[1] * numRacional2[1];
-            
-        return resultado;
+    public NumRacional Restar(NumRacional num, NumRacional num2){
+        int a = num.getNumerador();
+        int b = num.getDenominador();
+        int c = num2.getNumerador();
+        int d = num2.getDenominador();
+
+        this.setNumerador((a*d)-(b*c));
+        this.setDenominador(b*d);
+        return this;
     }
 
     
--- src/numracionalesmain/NumRacionalesMain.java
+++ src/numracionalesmain/NumRacionalesMain.java
@@ -8,17 +8,15 @@ public class NumRacionalesMain {
     public static void main(String[] args) {
         NumRacional num1 = new NumRacional();
         NumRacional num2 = new NumRacional(1,2);
-        NumRacional suma = new NumRacional().Sumar(num1, num2);
-        //int resultado [] = new int [2]; 
-
-        //resultado =num1.Sumar(num1, num2);
-
+        NumRacional operaciones = new NumRacional();
+    
+        operaciones.Sumar(num1, num2);
         System.out.println
-        ("La suma de: " + num1.getNumerador() + "/" + num1.getDenominador()+ " + " + num2.getNumerador() + "/" + num2.getDenominador() +  " es: " +  suma.getNumerador() + "/" + suma.getDenominador());
+        ("La suma de: " + num1.getNumerador() + "/" + num1.getDenominador()+ " + " + num2.getNumerador() + "/" + num2.getDenominador() +  " es: " +  operaciones.getNumerador() + "/" + operaciones.getDenominador());
 
-        //resultado =num1.Restar(num1, num2);
-       /*  System.out.println
-        ("La resta de: " + num1.getNumerador() + "/" + num1.getDenominador()+ " + " + num2.getNumerador() + "/" + num2.getDenominador() +  " es: " +  resultado[0] + "/" + resultado[1]); */
+        operaciones.Restar(num1, num2);
+        System.out.println
+        ("La resta de: " + num1.getNumerador() + "/" + num1.getDenominador()+ " - " + num2.getNumerador() + "/" + num2.getDenominador() +  " es: " +  operaciones.getNumerador() + "/" + operaciones.getDenominador());
     }
     
 }

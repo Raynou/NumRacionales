c58ca6089d474a2ad5a6ff1101ea01cc233a64b3
Ray <rafaelgame2012@gmail.com>
2021-03-26 12:53:13 -0600

    third commit

--- src/numracionalesmain/NumRacionalesMain.java
+++ src/numracionalesmain/NumRacionalesMain.java
@@ -8,8 +8,9 @@ public class NumRacionalesMain {
     public static void main(String[] args) {
         NumRacional num1 = new NumRacional();
         NumRacional num2 = new NumRacional(1,2);
-        NumRacional operaciones = new NumRacional();
-    
+        /* NumRacional operaciones = new NumRacional(); */
+        OperacionesNumRacionales operaciones = new OperacionesNumRacionales();
+        
         operaciones.Sumar(num1, num2);
         System.out.println
         ("La suma de: " + num1.getNumerador() + "/" + num1.getDenominador()+ " + " + num2.getNumerador() + "/" + num2.getDenominador() +  " es: " +  operaciones.getNumerador() + "/" + operaciones.getDenominador());

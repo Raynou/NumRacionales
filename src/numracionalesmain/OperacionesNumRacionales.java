cd5021cc97983bc2d45928907e0590a0930360e7
Ray <rafaelgame2012@gmail.com>
2021-03-26 13:29:38 -0600

    minor fixes

--- src/numracionalesmain/OperacionesNumRacionales.java
+++ src/numracionalesmain/OperacionesNumRacionales.java
@@ -3,23 +3,23 @@ package numracionalesmain;
 public class OperacionesNumRacionales extends NumRacional {
     
     public NumRacional Sumar(NumRacional num, NumRacional num2){
-        int a = num.getNumerador();
-        int b = num.getDenominador();
-        int c = num2.getNumerador();
-        int d = num2.getDenominador();
+        int primerNumerador = num.getNumerador(); 
+        int primerDenominador = num.getDenominador(); 
+        int segundoNumerador = num2.getNumerador();
+        int segundoDenominador = num2.getDenominador();
 
-        this.setNumerador((a*d)+(b*c));
-        this.setDenominador(b*d);
+        this.setNumerador((primerNumerador*segundoDenominador)+(primerDenominador*segundoNumerador));
+        this.setDenominador(primerDenominador*segundoDenominador);
         return this;
     }
     public NumRacional Restar(NumRacional num, NumRacional num2){
-        int a = num.getNumerador();
-        int b = num.getDenominador();
-        int c = num2.getNumerador();
-        int d = num2.getDenominador();
+        int primerNumerador = num.getNumerador(); 
+        int primerDenominador = num.getDenominador(); 
+        int segundoNumerador = num2.getNumerador();
+        int segundoDenominador = num2.getDenominador();
 
-        this.setNumerador((a*d)-(b*c));
-        this.setDenominador(b*d);
+        this.setNumerador((primerNumerador*segundoDenominador)-(primerDenominador*segundoNumerador));
+        this.setDenominador(primerDenominador*segundoDenominador);
         return this;
     }
 }

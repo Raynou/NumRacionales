5b59ad89c7fcb511ae6c8cf4331c7d246345febf
Ray <rafaelgame2012@gmail.com>
2021-03-26 12:55:11 -0600

    last commit, altern branch finished

--- src/numracionalesmain/NumRacional.java
+++ src/numracionalesmain/NumRacional.java
@@ -31,29 +31,6 @@ public class NumRacional {
 
     public void setDenominador(int denominador) {
         this.denominador = denominador;
-    }
-    
-    public NumRacional Sumar(NumRacional num, NumRacional num2){
-        int a = num.getNumerador();
-        int b = num.getDenominador();
-        int c = num2.getNumerador();
-        int d = num2.getDenominador();
-
-        this.setNumerador((a*d)+(b*c));
-        this.setDenominador(b*d);
-        return this;
-    }
-    public NumRacional Restar(NumRacional num, NumRacional num2){
-        int a = num.getNumerador();
-        int b = num.getDenominador();
-        int c = num2.getNumerador();
-        int d = num2.getDenominador();
-
-        this.setNumerador((a*d)-(b*c));
-        this.setDenominador(b*d);
-        return this;
-    }
-
-    
+    }    
     
 }
